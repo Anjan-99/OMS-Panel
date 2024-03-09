f0cb144499a3dc13c4f60f4e47bda8ad342cf754
ABHISHE KHUNT <103419190+Abhishekhunt@users.noreply.github.com>
2024-03-10 00:58:34 +0530

    ull

--- crypto/playground/views.py
+++ crypto/playground/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 #import binance module in try block
 from binance.client import Client
 from util import *
-#from ks_api_client import ks_api
+from ks_api_client import ks_api
 
 
 # Create your views here.
@@ -20,6 +20,9 @@ def index(request):
             exchange = str(request.POST.get('exchange'))
             symbol = str(request.POST.get('symbol'))
             stopLoss = float(request.POST.get('stopLoss'))
+            type = str(request.POST.get('type'))
+            lotsize = float(request.POST.get('lot_size'))
+            
             symbol = symbol.replace('/','')
 
             if exchange == 'Binance':
@@ -35,41 +38,43 @@ def index(request):
                         client = Client(api_key=binance_api_key, api_secret=binance_api_secret, tld='com')
                         price = client.get_symbol_ticker(symbol=symbol)
                         lot = client.get_symbol_info(symbol)
-                        lot = float(lot['filters'][1]['stepSize'])
-                        lot = str(10)
+                        lot = lotsize
                         price = price['price']
+                        
+                        
 
                         if stopLoss < float(price):
-                            try:
-                                order = client.create_test_order(
-                                    symbol=symbol,
-                                    side='BUY',
-                                    type='MARKET',
-                                    quantity= lot
+                            if type == 'Market':
+                                try:
+                                    order = client.create_test_order(
+                                        symbol=symbol,
+                                        side='BUY',
+                                        type='MARKET',
+                                        quantity= lot
+                                        )
+                                    print(order)
+                                    tracked[symbol] = {'exchange': exchange,'type': type ,'side': 'LONG', 'price': price, 'lotsize': lot, 'stopLoss': stopLoss}
+                                    #make json name tracked.json
+                                    with open('tracked.json', 'w') as f:
+                                        json.dump(tracked, f)
+                                except Exception as e:
+                                    return HttpResponse(f'Order Failed {e}')
+                            elif stopLoss > float(price):
+                                #place test order for short position
+                                try:
+                                    order = client.create_test_order(
+                                        symbol=symbol,
+                                        side='SELL',
+                                        type='MARKET',
+                                        quantity= lot
                                     )
-                                print(order)
-                                tracked[symbol] = {'exchange': exchange, 'side': 'LONG', 'stopLoss': stopLoss, 'price': price}
-                                #make json name tracked.json
-                                with open('tracked.json', 'w') as f:
-                                    json.dump(tracked, f)
-                            except Exception as e:
-                                return HttpResponse(f'Order Failed {e}')
-                        elif stopLoss > float(price):
-                            #place test order for short position
-                            try:
-                                order = client.create_test_order(
-                                    symbol=symbol,
-                                    side='SELL',
-                                    type='MARKET',
-                                    quantity= lot
-                                )
-                                print(order)
-                                tracked[symbol] = {'exchange': exchange, 'side': 'SHORT', 'stopLoss': stopLoss, 'price': price}
-                                #make json name tracked.json
-                                with open('tracked.json', 'w') as f:
-                                    json.dump(tracked, f)
-                            except Exception as e:
-                                return HttpResponse(f'Order Failed {e}')        
+                                    print(order)
+                                    tracked[symbol] = {'exchange': exchange,'type': type ,'side': 'SHORT', 'price': price, 'lotsize': lot, 'stopLoss': stopLoss}
+                                    #make json name tracked.json
+                                    with open('tracked.json', 'w') as f:
+                                        json.dump(tracked, f)
+                                except Exception as e:
+                                    return HttpResponse(f'Order Failed {e}')        
                     else:
                         e = 'Order for this symbol is already placed.'
                         return  render(request, 'hello.html', {'error': e})
