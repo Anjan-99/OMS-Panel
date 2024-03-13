9e413af00553fa4d82744eee62cb2a7897bc06fa
ABHISHE KHUNT <103419190+Abhishekhunt@users.noreply.github.com>
2024-03-13 22:23:54 +0530

    updated with break even and trailing sl
    Co-Authored-By: Anjan_knowns <88154256+Anjan-99@users.noreply.github.com>

--- crypto/playground/urls.py
+++ crypto/playground/urls.py
@@ -5,4 +5,6 @@ from django.urls import include
 urlpatterns = [ 
     path('',views.index,name="hello"),
     path('close_position/', views.close_position, name="close_position"),
+    path("b_e_position/", views.b_e_pos, name="b_e_position"),
+    path("modify_sl/", views.modify_sl, name="modify_sl"),
 ]
--- crypto/playground/views.py
+++ crypto/playground/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import json
 from binance.client import Client
 from util import *
+from binance.enums import *
+
+def fetch_data():
+    with open('tracked.json', 'r') as f:
+        tracked = json.load(f)
+    return tracked
 
 def index(request):
     if request.method == 'POST':
@@ -9,125 +15,255 @@ def index(request):
             print(request.POST)
             exchange = str(request.POST.get('exchange'))
             symbol = str(request.POST.get('symbol'))
-            stopLoss = float(request.POST.get('stopLoss'))
             types = str(request.POST.get('type'))
-            lotsize = float(request.POST.get('lot_size'))
+            stopLoss = float(request.POST.get('stopLoss'))
+            risk = float(request.POST.get('risk'))
+            #calculate lot size based on risk
+            
             symbol = symbol.replace('/','')
             if exchange == 'Binance':
 
                 if stopLoss > 0.0:
                         
                     #read the json
-                    with open('tracked.json', 'r') as f:
-                        tracked = json.load(f)
+                    tracked = fetch_data()
 
                     if symbol not in tracked:
                         
                         client = Client(api_key=binance_api_key, api_secret=binance_api_secret, tld='com')
+                        info = client.get_symbol_info(symbol)
                         price = client.get_symbol_ticker(symbol=symbol)
-                        lot = client.get_symbol_info(symbol)
-                        lot = lotsize
-                        price = price['price']
+                        price = float(price['price'])
+                        # get the tickSize from info
+                        tickSize = float(info['filters'][0]['tickSize'])
+                        print(tickSize)
                         
-                        if types == 'Market':
-                            if stopLoss < float(price):
+                        buylimitprice = float(price - tickSize)
+                        selllimitprice = float(price + tickSize)
+                        print(buylimitprice)
+                        print(selllimitprice)
+                        if types == 'Limit':
+                            if stopLoss < price:
                                 try:
+                                    lot = int(risk//(abs(buylimitprice - stopLoss)))
+                                    print(lot)
                                     order = client.create_test_order(
                                         symbol=symbol,
-                                        side='BUY',
-                                        type='MARKET',
-                                        quantity= lot
+                                        side= Client.SIDE_BUY,
+                                        type= Client.ORDER_TYPE_STOP_LOSS_LIMIT,
+                                        price= str(buylimitprice),
+                                        quantity= lot,
+                                        timeInForce = TIME_IN_FORCE_GTC,
+                                        stopPrice = stopLoss
                                         )
+                                    # order = client.create_order(
+                                    #     symbol=symbol,
+                                    #     side= Client.SIDE_BUY, 
+                                    #     type= Client.ORDER_TYPE_STOP_LOSS_LIMIT,
+                                    #     price= str(buylimitprice),
+                                    #     quantity= lot,
+                                    #     timeInForce = TIME_IN_FORCE_GTC,
+                                    #     stopPrice = stopLoss
+                                    # )
+                                    # orderid = order['orderId']
                                     print(order)
-                                    tracked[symbol] = {'exchange': exchange,'type': types ,'side': 'LONG', 'price': price, 'lotsize': lot, 'stopLoss': stopLoss}
+                                    tracked[symbol] = {'exchange': exchange,'orderID': 1 ,'type': types ,'side': 'LONG', 'price': str(buylimitprice), 'lotsize': lot, 'stopLoss': stopLoss}
                                     #make json name tracked.json
                                     with open('tracked.json', 'w') as f:
                                         json.dump(tracked, f)
                                 except Exception as e:
                                     #use rejex to get the error message
-                                    e = str(e).split(':')
-                                    e = e[1]
-                                    return render(request, 'hello.html', {'error': e,'order_history': tracked})
-                            elif stopLoss > float(price):
+                                    print(e)
+                                    return render(request, 'hello.html')
+                            elif stopLoss > price:
                                 #place test order for short position
                                 try:
+                                    lot = int(risk//(abs(selllimitprice - stopLoss)))
+                                    print(lot)
                                     order = client.create_test_order(
                                         symbol=symbol,
-                                        side='SELL',
-                                        type='MARKET',
-                                        quantity= lot
+                                        side=Client.SIDE_SELL,
+                                        type=Client.ORDER_TYPE_STOP_LOSS_LIMIT,
+                                        price= str(selllimitprice),
+                                        quantity= lot,
+                                        timeInForce = TIME_IN_FORCE_GTC,
+                                        stopPrice = stopLoss
                                     )
+                                    # order = client.create_order(
+                                    #     symbol=symbol,
+                                    #     side=Client.SIDE_SELL,
+                                    #     type=Client.ORDER_TYPE_STOP_LOSS_LIMIT,
+                                    #     price= str(selllimitprice),
+                                    #     quantity= lot,
+                                    #     timeInForce = TIME_IN_FORCE_GTC,
+                                    #     stopPrice = stopLoss
+                                    # )
+                                    # orderid = order['orderId']
                                     print(order)
-                                    tracked[symbol] = {'exchange': exchange,'type': types ,'side': 'SHORT', 'price': price, 'lotsize': lot, 'stopLoss': stopLoss}
+                                    tracked[symbol] = {'exchange': exchange,'orderID': 1,'type': types ,'side': 'SHORT', 'price': str(selllimitprice), 'lotsize': lot, 'stopLoss': stopLoss}
                                     #make json name tracked.json
                                     with open('tracked.json', 'w') as f:
                                         json.dump(tracked, f)
                                 except Exception as e:
-                                    e = str(e).split(':')
-                                    e = e[1]
-                                    return render(request, 'hello.html', {'error': e,'order_history': tracked})       
+                                    print(e)
+                                    return render(request, 'hello.html')       
                     else:
-                        e = 'Order for this symbol is already placed.'
-                        return  render(request, 'hello.html', {'error': e,'order_history': tracked})
-                    
-                
+                        return  render(request, 'hello.html')
             else:
-                e = 'Please select a valid exchange.'
-                return  render(request, 'hello.html', {'error': e,'order_history': tracked})
+                return  render(request, 'hello.html')
         except Exception as e:
-            e = 'please try again with valid inputs.'
-            return  render(request, 'hello.html', {'error': e,'order_history': tracked} )
+            print(e)
+            return  render(request, 'hello.html' )
 
-    with open('tracked.json', 'r') as f:
-        tracked = json.load(f)
-    return render(request, 'hello.html',{'order_history': tracked})
+    return render(request, 'hello.html')
 
 def close_position(request):
     if request.method == 'POST':
-        #read the json
-        with open('tracked.json', 'r') as f:
-            tracked = json.load(f)
-        seleted = request.POST.getlist('selected_symbols')
-        seleted = json.loads(seleted[0])
-        for symbol in seleted:
+        print(request.POST)
+        try:
+            #read the json
+            tracked = fetch_data()
+            print(tracked)
+            # check if the json is empty
+            if not tracked:
+                e = 'No position to close.'
+                return  render(request, 'hello.html' )
+            else:
+                # make a list of symbols from the json
+                symbollist = [symbol for symbol in tracked]
+                for symbol in symbollist:
+                    print(symbol)
+                    client = Client(api_key=binance_api_key, api_secret=binance_api_secret, tld='com')
+                    if tracked[symbol]['side'] == 'LONG':
+                        try:
+                            order = client.create_test_order(
+                                symbol=symbol,
+                                side='SELL',
+                                type='MARKET',
+                                quantity= tracked[symbol]['lotsize']
+                            )
+                            # order = client.cancel_order(
+                            #     symbol=symbol,
+                            #     orderId=tracked[symbol]['orderID']
+                            # )
+                            print(order)
+                            del tracked[symbol]
+                            with open('tracked.json', 'w') as f:
+                                json.dump(tracked, f)
+                        except Exception as e:
+                            print(e)
+                            return render(request, 'hello.html')
+                    elif tracked[symbol]['side'] == 'SHORT':
+                        print(symbol)
+                        try:
+                            order = client.create_test_order(
+                                symbol=symbol,
+                                side=Client.SIDE_BUY,
+                                type='MARKET',
+                                quantity= tracked[symbol]['lotsize']
+                            )
+                            # order = client.cancel_order(
+                            #     symbol=symbol,
+                            #     orderId=tracked[symbol]['orderID']
+                            # )
+                            print(order)
+                            del tracked[symbol]
+                            with open('tracked.json', 'w') as f:
+                                json.dump(tracked, f)
+                            
+                        except Exception as e:
+                            print(e)
+                            return render(request, 'hello.html')
+                    
+        except Exception as e:
+            return  render(request, 'hello.html' )
+
+    return  render(request ,'hello.html' )
+
+
+def b_e_pos(request):
+    # break even position modify stoploss and move it to entry price and modifiy the order
+    if request.method == 'POST':
+        try:
+            #read the json
+            tracked = fetch_data()
+            print(tracked)
+            #get the first symbol from the json
+            symbol = list(tracked.keys())[0]
             print(symbol)
-            client = Client(api_key=binance_api_key, api_secret=binance_api_secret, tld='com')
-            price = client.get_symbol_ticker(symbol=symbol)
-            price = price['price']
-            if tracked[symbol]['side'] == 'LONG':
+            # check if the json is empty
+            if not tracked:
+                return  render(request, 'hello.html' )
+            else:
+                # make a list of symbols from the json
+                #modify the order
+                client = Client(api_key=binance_api_key, api_secret=binance_api_secret, tld='com')
+                
+                stoploss = tracked[symbol]['price']
+                tracked[symbol]['stopLoss'] = stoploss
                 try:
-                    order = client.create_test_order(
-                        symbol=symbol,
-                        side='SELL',
-                        type='MARKET',
-                        quantity= tracked[symbol]['lotsize']
-                    )
-                    print(order)
-                    del tracked[symbol]
+                    print("order modified successfully.")
                     with open('tracked.json', 'w') as f:
                         json.dump(tracked, f)
-                    return  render(request, 'hello.html',{'order_history': tracked})
                 except Exception as e:
-                    e = str(e).split(':')
-                    e = e[1]
-                    return render(request, 'hello.html', {'error': e,'order_history': tracked})
-            elif tracked[symbol]['side'] == 'SHORT':
-                try:
-                    order = client.create_test_order(
-                        symbol=symbol,
-                        side='BUY',
-                        type='MARKET',
-                        quantity= tracked[symbol]['lotsize']
-                    )
-                    print(order)
-                    del tracked[symbol]
-                    with open('tracked.json', 'w') as f:
-                        json.dump(tracked, f)
-                    return  render(request, 'hello.html',{'order_history': tracked})
-                except Exception as e:
-                    e = str(e).split(':')
-                    e = e[1]
-                    return render(request, 'hello.html', {'error': e,'order_history': tracked})
-    with open('tracked.json', 'r') as f:
-        tracked = json.load(f)
-    return  render(request, 'hello.html',{'order_history': tracked})
+                    print(e)
+                    return render(request, 'hello.html')
+                
+                    
+        except Exception as e:
+            print(e)
+            return  render(request, 'hello.html', {'error1': e} )
+
+    return  render(request ,'hello.html' )
+
+def modify_sl(request):
+    if request.method == 'POST':
+        try:
+            #read the json
+            tracked = fetch_data()
+            print(tracked)
+            #get the first symbol from the json
+            print(request.POST)
+            
+            exchange = str(request.POST.get('exchange'))
+            symbol = str(request.POST.get('symbol'))
+            modify_stopLoss = float(request.POST.get('modifysl'))
+            
+            symbol = symbol.replace('/','')
+            if exchange == 'Binance':
+                if modify_stopLoss > 0.0:
+                    if symbol in tracked:
+                        client = Client(api_key=binance_api_key, api_secret=binance_api_secret, tld='com')
+                        
+                        tracked[symbol]['stopLoss'] = modify_stopLoss
+                        try:
+                            print("order modified successfully.")
+                            with open('tracked.json', 'w') as f:
+                                json.dump(tracked, f)
+                        except Exception as e:
+                            print(e)
+                            return render(request, 'hello.html')
+                    else:
+                        e = 'No position to modify.'
+                        print(e)
+                        return  render(request, 'hello.html' )
+                else:
+                    e = 'Please enter a valid stoploss.'
+                    print(e)
+                    return  render(request, 'hello.html')
+                
+            # elif exchange == 'Bybit':
+            #     e = 'Please select a valid exchange.'
+            #     tracked = fetch_data()
+            #     return  render(request, 'hello.html')
+            
+        except Exception as e:
+            print(e)
+            return  render(request, 'hello.html' )
+        
+    return  render(request ,'hello.html' )
+            
+                        
+                        
+                        
+            
